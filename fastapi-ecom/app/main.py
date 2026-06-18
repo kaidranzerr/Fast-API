d974fb9cfbace872213a453376c04dd2c494720e
kaidranzerr <ashstark68@gmail.com>
2026-06-18 17:25:54 +0530

    dynamic routes done!

--- fastapi-ecom/app/main.py
+++ fastapi-ecom/app/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , HTTPException
 app = FastAPI()
 
 
-# first endpoint
+# first endpoint --> static route
 @app.get('/')
 def root():
     return {"message": "Welcome to main bitch"}
+
+
+# dynamic route
+@app.get("/products/{id}")
+def get_products(id: int):
+    products = ['abc' , 'def' , 'fgh' , 'ijk']
+    return products[id] if products[id] else HTTPException(status_code=404 ,detail= "No product found!")
+
--- stuff.py
+++ stuff.py
@@ -1,3 +1,4 @@
 # from backed we create endpoints and through APIs we access frontend and backend
 # pydantic(validation) + starlite(request + response) facilities using HTTPs
-
+# GET , POST , PUT , DELETE
+# CRUD --> create read update delete 
