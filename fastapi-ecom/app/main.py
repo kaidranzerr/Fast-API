312eb4a94b0af41269826954638c197b07a20291
kaidranzerr <ashstark68@gmail.com>
2026-06-18 17:41:59 +0530

    products loaded bitch!!!

--- fastapi-ecom/app/main.py
+++ fastapi-ecom/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI , HTTPException
+from .services.products import get_all_products
 app = FastAPI()
 
 
@@ -8,9 +9,12 @@ def root():
     return {"message": "Welcome to main bitch"}
 
 
-# dynamic route
-@app.get("/products/{id}")
-def get_products(id: int):
-    products = ['abc' , 'def' , 'fgh' , 'ijk']
-    return products[id] if products[id] else HTTPException(status_code=404 ,detail= "No product found!")
+# # dynamic route
+# @app.get("/products/{id}")
+# def get_products(id: int):
+#     products = ['abc' , 'def' , 'fgh' , 'ijk']
+#     return products[id] if products[id] else HTTPException(status_code=404 ,detail= "No product found!")
 
+@app.get("/products")
+def get_products():
+    return get_all_products()
